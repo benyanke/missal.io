6df6bdcba99b00a2f95495d4c60b8272f46838f6
Ben Yanke <byanke@pcnametag.com>
2018-02-04 23:53:52 -0600

    basic proof of concept

--- vendor/app.js
+++ vendor/app.js
@@ -8,14 +8,17 @@ var app = new Vue({
     info: [],
 
     // Set to true to retrieve all texts from local cache when possible
-    //
     caching: false,
 
     // When set to true, will hide incomplete mass texts
     hideIncomplete: true,
 
     // Will be set to true when index is loaded
-    appReady: false
+    appReady: false,
+
+    // This will be null if no mass is selected
+    // if a mass is selected, place it here and it will be rendered
+    currentLiturgy: null
 
   },
   mounted: function(){
@@ -39,6 +42,7 @@ var app = new Vue({
         console.log("Index retrieved of " + app.indexContent.length + " texts.");
 
         app.getLiturgy("lent/ash/wed");
+
       // })
       //.catch(function (error) {
       //  console.log(error);
@@ -46,21 +50,37 @@ var app = new Vue({
 
     },
 
+    // Searches an array of objects for an object with a given key = val
+    getElementByKey: function(srcArray, key, value) {
+      return srcArray.find(arr => arr[key] === value);
+    },
+
     // Gets an object containing the entire liturgy, by it's slug
     getLiturgy: function(slugToLookup) {
 
       console.log("Getting liturgy by slug '" + slugToLookup + "'")
 
+      // Check if available in cache first
+      cachedValue = this.getCacheValueBySlug(slugToLookup);
+      if (cachedValue != null) {
+        app.currentLiturgy = cachedValue;
+      }
+
       // Find the element in the index
       var indexEntry = this.indexContent.find(index => index.slug === slugToLookup);
-      console.log(indexEntry);
       file = this.info.hrefBase + indexEntry.href;
 
       // Look it up by it's found path
       axios.get(file)
       .then(function (response) {
           console.log("Found data for " + response.data.name + ":", response.data);
+          app.currentLiturgy = response.data;
+      })
+      .catch(function (error) {
+        console.log("HTTP error: ", error);
       });
+
+
     },
 
     // Stores a mass text into the cache
@@ -69,8 +89,9 @@ var app = new Vue({
     },
 
     // Retrieves a mass text into the cache
+    // Returns null if cached value is expired or not existing
     getCacheValueBySlug: function(slug) {
-
+        return null;
     }
 
   }
